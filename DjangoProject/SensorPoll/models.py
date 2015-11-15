1d363c2ecaf753ac844ea27d121b156dd9c8b92f
Max Lam <lamothe.max@gmail.com>
2015-11-15 13:00:42 -0500

    modified models to automatically timestamp

--- DjangoProject/SensorPoll/models.py
+++ DjangoProject/SensorPoll/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+﻿from django.db import models
 
 class SensorPacket(models.Model):
 	address = models.IntegerField()
-	time_stamp = models.DateTimeField('date received')
+	time_stamp = models.DateTimeField(auto_now_add=True)
 	data = models.IntegerField()
