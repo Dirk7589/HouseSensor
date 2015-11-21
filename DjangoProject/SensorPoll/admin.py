a19d97a28f6bef411d5063868509b4d7c8eefb7b
Max Lam <lamothe.max@gmail.com>
2015-11-21 01:36:51 -0500

    Admin page modification to include averaged data model

--- DjangoProject/SensorPoll/admin.py
+++ DjangoProject/SensorPoll/admin.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 
 from .models import SensorPacket
+from .models import AveragedData
 
 admin.site.register(SensorPacket)
 # Register your models here.
+admin.site.register(AveragedData)
