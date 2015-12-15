6495ef9024468f0d46a158011207e3d39175ecaf
Max Lam <lamothe.max@gmail.com>
2015-12-15 16:02:32 -0500

    Made changes to views to simplify/clarify code. Also made changes to models to add string description used in admin view.

--- DjangoProject/SensorPoll/models.py
+++ DjangoProject/SensorPoll/models.py
@@ -1,11 +1,23 @@
 ﻿from django.db import models
 
 class SensorPacket(models.Model):
-	address = models.IntegerField()
-	time_stamp = models.DateTimeField(auto_now_add=True)
-	data = models.DecimalField(max_digits=6, decimal_places=2)
 
-class AveragedData(models.Model):
     address = models.IntegerField()
     time_stamp = models.DateTimeField(auto_now_add=True)
     data = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return ('address: ' + str(self.address) + ', Time: ' + str(self.time_stamp) + ', Data: '+ str(self.data))
+
+
+
+
+
+class AveragedData(models.Model):
+
+    address = models.IntegerField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    data = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return ('address: ' + str(self.address) + ',    Time: ' + str(self.time_stamp) + ',    Data: '+ str(self.data))
--- DjangoProject/SensorPoll/views.py
+++ DjangoProject/SensorPoll/views.py
@@ -1,19 +1,17 @@
 ﻿from django.http import Http404
 from django.shortcuts import render
-from django.template import RequestContext, loader
 from .models import SensorPacket
 from .models import AveragedData
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 
 def index(request):
-	template = loader.get_template('SensorPoll/index.html')
 	packets = SensorPacket.objects.all()
-	context = RequestContext(request, {'packets':packets,})
-	return HttpResponse(template.render(context))
+	context = {'packets':packets,}
+	return render(request, 'SensorPoll/index.html', context)
 
 def tables(request):
-    template = loader.get_template('SensorPoll/tables.html')
+
     packets_list = SensorPacket.objects.all()
     paginator = Paginator(packets_list, 25)
     page = request.GET.get('page')
@@ -25,15 +23,14 @@ def tables(request):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         packets = paginator.page(paginator.num_pages)
-    context = RequestContext(request, {'packets':packets,})
-    return HttpResponse(template.render(context))
+
+    context = {'packets':packets,}
+    return render(request, 'SensorPoll/tables.html', context)
 
 def charts(request):
-    template = loader.get_template('SensorPoll/charts.html')
-    #packets = SensorPacket.objects.all()
+    
     averaged_packs = AveragedData.objects.all()
-    #context = RequestContext(request, {'packets':packets,})
-    context = RequestContext(request, {'averaged_packs':averaged_packs,})
-    return HttpResponse(template.render(context))
+    context = {'averaged_packs':averaged_packs,}
+    return render(request, 'SensorPoll/charts.html', context)
 
 
