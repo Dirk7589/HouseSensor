0547b86a267ba79f4ae1fd39bf30d68feebae1a7
Max Lam <lamothe.max@gmail.com>
2015-12-15 19:53:15 -0500

    Added new model for ActiveSensors

--- DjangoProject/SensorPoll/admin.py
+++ DjangoProject/SensorPoll/admin.py
@@ -2,7 +2,9 @@
 
 from .models import SensorPacket
 from .models import AveragedData
+from .models import ActiveSensors
 
 admin.site.register(SensorPacket)
 # Register your models here.
 admin.site.register(AveragedData)
+admin.site.register(ActiveSensors)
--- DjangoProject/SensorPoll/models.py
+++ DjangoProject/SensorPoll/models.py
@@ -21,3 +21,10 @@ class AveragedData(models.Model):
 
     def __str__(self):
         return ('address: ' + str(self.address) + ',    Time: ' + str(self.time_stamp) + ',    Data: '+ str(self.data))
+
+class ActiveSensors(models.Model):
+    Sensor_number = models.IntegerField()
+    Sensor_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Sensor_name
